5572c04650d678d37fb421143410d2cdaf6933dd
Gonzalo <goibacache@gmail.com>
2024-03-03 18:54:04 -0300

    ReactApp working

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+  //res.render('index', { title: 'Bloon' });
+});
+
+module.exports = router;
