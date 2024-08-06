d5aba86e2b16d0e8f65529a5c3b472ce05a36404
Gonzalo <goibacache@gmail.com>
2024-08-06 09:59:15 -0400

    Update from jade to pug engine

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // Enable/disable logger based on env argv length
 if (process.argv.length <= 2){ // Two means it's dev
