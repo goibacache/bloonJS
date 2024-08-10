35c2aecadf7e2ba424f65754d50d46bad5d512d3
Gonzalo <goibacache@gmail.com>
2024-08-10 14:10:37 -0400

    Dynamic calendar in JS

--- src/public/js/schedule.js
+++ src/public/js/schedule.js
@@ -1,67 +1,175 @@
-let calendar = null;
-let mode = "add";
+$(document).ready(() => {
 
-const loadCalendar = (clean=false) => {
-    calendar = $('#calendar');
+    let calendar = null;
+    let mode = "add";
 
-    //Clean calendar
-    if (clean){
-        calendar.children().remove();
-    }
+    const loadCalendar = (timezone = "America/Santiago", clean = true) => {
+        calendar = $('#calendar');
 
-    // Remember: YYYY/DD/MM 🙄
-    const StartOfSchedule = spacetime('2024/07/08', 'America/Los_Angeles').goto('America/Santiago');
-    console.log(StartOfSchedule.format('time'));
-    
-    
-}
-
-loadCalendar();
-
-
-
-// Mark calendar
-$(".selectableDate").on('mouseenter', (e) => {
-    if (e.originalEvent.buttons > 0){ // more than one button that is the right click
-        if (mode == "remove"){
-            $(e.currentTarget).removeClass('mySelection');
-        }else{
-            $(e.currentTarget).addClass('mySelection');
+        //Clean calendar
+        if (clean) {
+            calendar.children().remove();
+            calendar.append('<thead></thead>');
+            $("#calendar thead").append("<tr></tr>");
+            calendar.append('<tbody></tbody>');
+            
         }
-    }
-});
 
-$(".selectableDate").on('mousedown', (e) => {
-    if ($(e.currentTarget).hasClass('mySelection')){
-        mode = "remove";
-        $(e.currentTarget).removeClass('mySelection');
-    }else{
-        mode = "add";
-        $(e.currentTarget).addClass('mySelection');
-    }
-});
+        /*
+            Remember: YYYY/M/D 
+            Month: 0 based 
+            Days: 1 based
+            🙄 IK
+        */
+       
+        const startDate = spacetime([2024, 7, 9], 'America/Los_Angeles').goto(timezone); // 2024-08-09
+        const localStartDate = spacetime([2024, 7, 9], timezone);
+        //const endDate = spacetime([2024, 7, 9], 'America/Los_Angeles').add(3, 'days').goto(timezone); // 2024-08-09
+        //let currentDate = startDate;
 
-// Mark filter
-$(".toggleCalendarVisibility").on('click, mousedown', (e) => {
-    const visibilityToHide = $(e.currentTarget).data("toggle");
+        /**
+         * Holds an array of days to draw
+         */
+        const days = buildDayArray(4, startDate);
 
-    if ($(e.currentTarget).hasClass('forceInactive')){
-        mode = "remove";
-        $(`.active${visibilityToHide}`).removeClass('forceInactive');
-    }else{
-        mode = "add";
-        $(`.active${visibilityToHide}`).addClass('forceInactive');
-    }
-});
+        // Create headers
 
-$(".toggleCalendarVisibility").on('mouseenter', (e) => {
-    if (e.originalEvent.buttons > 0){ // more than one button that is the right click
-        const visibilityToHide = $(e.currentTarget).data("toggle");
+        // Empty spacer
+        $('#calendar thead tr').append(`<th class="text-center borderL t-time" style="background-color:black"></th>`); 
+        days.forEach(day => {
+            // If the last day starts at midnight, skip it 'cause it's the end.
+            if (days.indexOf(day) == days.length-1){
+                if (day.startTime == "12:00am"){
+                    return;
+                }
+            }
+            $('#calendar thead tr').append(`
+                <th class="text-center borderL t-time" style="background-color:black">
+                    <p class="m-0 small">${day.monthDay}</p>
+                    <p class="m-0 small">${day.nameOfDay}</p>
+                </th>`
+            );
+        });
 
-        if (mode == "remove"){
-            $(`.active${visibilityToHide}`).removeClass('forceInactive');
-        }else{
-            $(`.active${visibilityToHide}`).addClass('forceInactive');
+        for (var i = 0; i < 96; ++i) { // Amount of hours/rows
+
+            // Create TR
+            const currentTr = document.createElement('tr');
+
+            // Create first column only on the first iteration
+            if (i % 4 == 0){
+                currentTr.innerHTML += `
+                <td rowspan="4" class="calendarCell borderH text-center timeText" style="background-color:black;">
+                    ${i/4}:00
+                </td>`;
+            }
+
+            // For each day, create a TD
+            days.forEach(day => {
+                // If the last day starts at midnight, skip it 'cause it's the end.
+                if (days.indexOf(day) == days.length-1 && day.startTime == "12:00am"){
+                    return;
+                }
+                //if (i % 4 != 0){
+                    // Create current calendar space
+                    currentTr.innerHTML += `
+                    <td class="calendarCell borderL text-center">
+                        <div class="selectableDate">
+                        </div>
+                    </td>`;
+                //}
+                
+            });
+
+            $("#calendar tbody").append(currentTr);
         }
+            
+
+        console.log("days:", days);
+        console.log("game date:", startDate.format('time-'));
+        console.log("local start date:", localStartDate);
+
+        
     }
+
+    const buildDayArray = (amountOfDays = 3, startDate) => {
+        const days = [];
+
+        for (let i = 0; i < amountOfDays; i++) {
+            const currentDate = startDate.add(i, 'days')
+            days.push({
+                monthDay: `${currentDate.format('month-short')} ${currentDate.format('date')}`,
+                nameOfDay: currentDate.format('day'),
+                startTime: currentDate.format('time')
+            });
+        }
+        
+        return days;
+    }
+
+
+    // Load tooltips
+    const loadTooltips = () => {
+        $('[data-toggle="tooltip"]').tooltip({
+            html: true
+        });
+    }
+
+    const handleMarks = () => {
+        // Mark calendar
+        $(".selectableDate").on('mouseenter', (e) => {
+            if (e.originalEvent.buttons > 0) { // more than one button that is the right click
+                if (mode == "remove") {
+                    $(e.currentTarget).removeClass('mySelection');
+                } else {
+                    $(e.currentTarget).addClass('mySelection');
+                }
+            }
+        });
+
+        $(".selectableDate").on('mousedown', (e) => {
+            if ($(e.currentTarget).hasClass('mySelection')) {
+                mode = "remove";
+                $(e.currentTarget).removeClass('mySelection');
+            } else {
+                mode = "add";
+                $(e.currentTarget).addClass('mySelection');
+            }
+        });
+    }
+
+    const handleVisibilityButtons = () => {
+        // Mark filter
+        $(".toggleCalendarVisibility").on('click, mousedown', (e) => {
+            const visibilityToHide = $(e.currentTarget).data("toggle");
+
+            if ($(e.currentTarget).hasClass('forceInactive')) {
+                mode = "remove";
+                $(`.active${visibilityToHide}`).removeClass('forceInactive');
+            } else {
+                mode = "add";
+                $(`.active${visibilityToHide}`).addClass('forceInactive');
+            }
+        });
+
+        $(".toggleCalendarVisibility").on('mouseenter', (e) => {
+            if (e.originalEvent.buttons > 0) { // more than one button that is the right click
+                const visibilityToHide = $(e.currentTarget).data("toggle");
+
+                if (mode == "remove") {
+                    $(`.active${visibilityToHide}`).removeClass('forceInactive');
+                } else {
+                    $(`.active${visibilityToHide}`).addClass('forceInactive');
+                }
+            }
+        });
+    }
+
+
+
+    // On start functions
+    handleMarks();
+    handleVisibilityButtons();
+    loadCalendar();
+    loadTooltips();
 });
