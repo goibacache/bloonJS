fce56f269148db8d5284a4142d328ae46d19600e
Gonzalo <goibacache@gmail.com>
2024-10-03 22:02:01 -0300

    Allow for time format change

--- src/public/js/general.js
+++ src/public/js/general.js
@@ -16,12 +16,32 @@ const logOut = async () => {
 }
 
 const makeOptionsDoNothing = () => {
-    $('.dropdown-menu a').on('click', function(e) {
+    $('.doNothing').on('click', function(e) {
         e.stopPropagation();
     });
 }
 
+const setTimeFormat = () => {
+    const format = $("input[name=12or24selector]:checked").val();
+    localStorage.setItem('timeFormat',format);
+}
+
+const loadTimeFormat = () => {
+    let timeFormat = localStorage.getItem('timeFormat');
+    if (timeFormat == undefined || timeFormat == null || timeFormat.length == 0){
+        timeFormat = "24hr";
+        localStorage.setItem('timeFormat', timeFormat);
+    }
+    return timeFormat;
+}
+
+const selectTimeFormat = (timeFormat) => {
+    $(`input[name='12or24selector'][value='${timeFormat}']`).prop('checked', true);
+}
+
 $(() => {
     makeOptionsDoNothing();
+    const timeFormat = loadTimeFormat();
+    selectTimeFormat(timeFormat);
     isLoading = false;
 });
