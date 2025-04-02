135eac9503b433dd8ac6b2b5e435daa27981c6c3
Gonzalo <goibacache@gmail.com>
2025-04-01 22:26:06 -0300

    Added ICL serverID to config.example.js

--- src/config.example.js
+++ src/config.example.js
@@ -57,9 +57,10 @@ module.exports = {
 	WEB_Port:						"80",
 
 	// ICL site config
-	oAutClientId:					"your-discord-bot-client-id", 				// Your bot's oAuth
-	oAutClientSecret:				"your-discord-bot-client-secret", 			// Your bot's client's oAuth
-	oAuthReturnUrl:					"your-discord-bot-oauth-return-url",		// I deploy the site on bloon.local but you can wherever
+	ICLServerId:					"your-icl-server-id",
+	oAutClientId:					"your-discord-bot-client-id", 				// Your bot's oAuth Client Id
+	oAutClientSecret:				"your-discord-bot-client-secret", 			// Your bot's oAuth Client Secret
+	oAuthReturnUrl:					"your-discord-bot-oauth-return-url",		// I deploy the site on bloon.local/authorize but you can wherever
 	oAuthTokenSecret:				"your-discord-bot-auth-token-to-sign-jwts",	// RSA key or a bunch of random strings for testing
 	ICLWebHookToAddToTeam:			"your-discord-webhook-url-so-people-can-tell-you-they-want-to-join-a-team-but-are-banned-discord.com/api-is-pre-appended"	
 }
