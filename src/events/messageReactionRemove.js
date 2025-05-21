dc9b7c0f2ef27378b458d09352250138053135ce
Gonzalo <goibacache@gmail.com>
2025-05-21 11:16:53 -0400

    Message reaction remove

--- src/events/messageReactionRemove.js
+++ src/events/messageReactionRemove.js
@@ -1,18 +1,31 @@
 const { Events } = require('discord.js');
-const bloonUtils = require('../utils/utils.js');
-const config = bloonUtils.getConfig();
+// eslint-disable-next-line no-unused-vars
+const { ServerConfig }              = require('../interfaces/ServerConfig.js'); // Used so VSCODE can see the properties
 
 const evnt = {
     name: Events.MessageReactionRemove,
 	async execute(reaction, user) {
 		try{
-			if (reaction.message.id == config.newsSubscribeMessageId){
-				console.log(`News message un-reacted ${user.id}`);
+            /**
+             * The server config
+             * @type {ServerConfig}
+             */
+            const serverConfig = reaction.client.serverConfigs.find(x => x.ServerId == reaction.guild.id);
+            if (!serverConfig){
+                console.log(`Reaction Remove: No config found for guild ${reaction.guild.id} for removing reaction.`);
+                return;
+            }
 
-				const   member      = reaction.message.guild.members.cache.get(user.id);  // Get current member
-				const   newsRole    = await reaction.message.guild.roles.fetch(config.role_News);
-				if (member.roles.cache.some(role => role.id === config.role_News)){
-					await member.roles.remove(newsRole);   // Remove
+            console.log(`Reaction Remove: Using config of ${serverConfig.ServerName}/(${serverConfig.ServerId})`);
+
+
+			if (reaction.message.id == serverConfig.MRA_MessageIdToReact){
+				console.log(`Reaction Remove: Message un-reacted by ${user.id}`);
+
+				const   member          = reaction.message.guild.members.cache.get(user.id);  // Get current member
+				const   reactionRole    = await reaction.message.guild.roles.fetch(serverConfig.MRA_RoleToToggleOnReact);
+				if (member.roles.cache.some(role => role.id === serverConfig.MRA_RoleToToggleOnReact)){
+					await member.roles.remove(reactionRole);   // Remove
 				}
 			}			
 		}catch(error){
